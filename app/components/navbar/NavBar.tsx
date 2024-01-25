4f0f4f9b1ce49ba8413cdde44df466b0786150f7
Fulop-Krisztian <50krissssz22@gmail.com>
2024-01-25 16:17:20 +0100

    Minor fix for deployment

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -12,12 +12,12 @@ const NavBar = () => {
             <Link href='/'><img src="favicon.ico" alt="" className='max-h-10' /></Link>
             <ul className='flex space-x-6'>
                 {links.map(link =>
-                    <li><Link
-                        key={link.href}
+                    <li key={link.href}>
+                        <Link
                         className='text-zinc-500 hover:text-zinc-800 transition-colors'
                         href={link.href}>
                         {link.name}
-                    </Link></li>
+                        </Link></li>
                 )}
             </ul>
         </nav>
