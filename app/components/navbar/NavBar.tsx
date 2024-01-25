85e259a20845c2a5909934f37d1824a368a9bb1c
Fulop-Krisztian <50krissssz22@gmail.com>
2024-01-25 17:09:29 +0100

    Prisma integrálása és konfigurálása

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const NavBar = () => {
+    // Jelenlegi útvonal lekérdezése
+    const currentPath = usePathname();
+ 
+    // Itt lehet megadni a navbar linkeket
     const links = [
+        // name: Amit kiír, href: Ahova vezet
         { name: 'Termékek', href: '/products' },
         { name: 'Fórum', href: '/forum' },
     ]
@@ -14,7 +22,14 @@ const NavBar = () => {
                 {links.map(link =>
                     <li><Link
                         key={link.href}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                        className=
+
+                            // Hogyha a jelenlegi oldal egyenlő azzal amire a menüpont mutat, akkor sötétebb a szöveg
+                            {`${link.href === currentPath ? 'text-zinc-900' : 'text-zinc-500'}
+
+                            // További stílusok
+                            hover:text-zinc-800 transition-colors`}
+                            
                         href={link.href}>
                         {link.name}
                     </Link></li>
