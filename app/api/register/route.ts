29280e12b5a02e496ae11fad66d9838a03c59f3d
Fulop-Krisztian <50krissssz22@gmail.com>
2024-01-28 13:06:47 +0100

    Működő pass. hash

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,14 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 import prisma from "@/prisma/client";
+import { hash } from 'bcrypt';
 
+// Az adatok amik kellenek a regisztációhoz 
 const registerSchema = z.object({
     // A beérkező adat struktúrája ennek meg kell hogy feleljen:
     username: z.string().min(3, { message: "A felhasználónévnek legalább 3 karakternek kell lennie" }).max(30, { message: "A felhasználónév maximum 30 karakter lehet" }),
     email: z.string().min(1, { message: "Az E-mail mező kötelező" }).max(255, { message: "Az E-mail maximum 255 karakter hosszú lehet" }).email({ message: "Hibás E-mail formátum" }),
     password: z.string().min(4, { message: "A jelszónak legalább 4 karakter hosszúnak kell lennie" }),
 });
-
 
 // Ha jön egy POST request akkor...
 export async function POST(request: NextRequest) {
@@ -18,32 +19,41 @@ export async function POST(request: NextRequest) {
         // validáljuk a regisztrációs adatokat
         const validation = registerSchema.safeParse(body);
 
+        // ha sikertelen elküldjük az okokat
         if (!validation.success) {
             return NextResponse.json(validation.error.errors, { status: 400 });
         }
 
+        // felbontjuk a requestet
         const { username, email, password } = body;
 
+        // email egyediséget ellenőrizzük
         const existingUser = await prisma.user.findUnique({
             where: {
                 email,
             },
         });
-
+        // ha nem egyedi hibát küldünk
         if (existingUser) {
             return NextResponse.json({ error: 'Már létezik ilyen e-mail címmel fiók' }, { status: 400 });
         }
 
+        // Declare hashedPassword before using it
+        let hashedPassword = await hash(password, 10);
+
+        // ha minden jó létrehozzuk az új felhasználót
         const newUser = await prisma.user.create({
             data: {
                 username,
                 email,
-                password,
+                hashedPassword,
             },
         });
-
+        // visszaküldjük a siker jelét
         return NextResponse.json(newUser, { status: 201 });
-    } catch (error: any) {
+    }
+    // Ha közben baj van akkor server errort küldünk (ilyen nem kéne hogy történjen normál esetben)
+    catch (error: any) {
         console.error("Error during registration:", error.errors || error.message);
         return NextResponse.json('Error during registration', { status: 500 });
     }
