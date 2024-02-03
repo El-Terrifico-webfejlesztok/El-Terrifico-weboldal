9efd93dd4abc5e98dd8388eeb358a5d3f77c21ea
Fulop-Krisztian <50krissssz22@gmail.com>
2024-02-03 22:08:33 +0100

    upload API 1. verzió

---
+++ app/api/product/upload/route.ts
@@ -0,0 +1,39 @@
+import prisma from "@/prisma/client";
+import { z } from 'zod';
+import { NextRequest, NextResponse } from 'next/server';
+
+const uploadProductSchema = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().min(1),
+  price: z.number(),
+  stock: z.number(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const validation = uploadProductSchema.safeParse(body);
+
+    if (!validation.success) {
+      return NextResponse.json(validation.error.errors, { status: 400 });
+    }
+
+    const newProduct = await prisma.product.create({
+      data: {
+        name: body.name,
+        description: body.description,
+        price: body.price,
+        stock: body.stock,
+      }
+    });
+
+    console.log('Product upload successful');
+    return NextResponse.json(newProduct, { status: 201 })
+  }
+  catch (error) {
+    console.error('Error during product upload:', error);
+    return NextResponse.json('Error during product upload', { status: 500 });
+  }
+};
+
