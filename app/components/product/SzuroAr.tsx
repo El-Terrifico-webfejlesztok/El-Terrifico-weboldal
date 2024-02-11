3fd2aba33f4b6e6b2ad55236eb340bd4c13c7733
Fulop-Krisztian <50krissssz22@gmail.com>
2024-02-11 22:59:15 +0100

    Product oldal keresés I. verzió (név, min-max ár, kategória) + kép útvonal visszaküldés az API-ban

--- app/components/product/SzuroAr.tsx
+++ app/components/product/SzuroAr.tsx
@@ -6,15 +6,17 @@ interface props {
     placeh: string
     minertek: number
     cim: string
+    name:string
 }
 
-function SzuroAr({placeh, minertek, cim}: props) {
+function SzuroAr({placeh, minertek, cim, name}: props) {
   return (
     <div className='mb-10'>
       <div className="label items-center justify-center">
         <span className="label-text">{cim}</span>
       </div>
       <input
+        name={name}
         type="number"
         placeholder={placeh}
         className={styles.ardoboz}
