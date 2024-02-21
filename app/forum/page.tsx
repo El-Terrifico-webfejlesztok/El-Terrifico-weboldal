9d0e6f294d8a166524d51d6a8a374db74debbb96
nagy-barnabas <barni.nagy2004@gmail.com>
2024-02-21 20:08:03 +0100

    Forum: az eddig felmerült hibák javítása

--- app/forum/page.tsx
+++ app/forum/page.tsx
@@ -37,7 +37,7 @@ const Forum = () => {
 
   return (
     <div className={styles.forumOldal}>
-      <div className="navbar bg-green-500 w-5/6 mx-auto shadow-sm rounded-md">
+      <div className="navbar bg-success sm:w-4/5 w-1/1 mx-auto shadow-sm rounded-md">
         <div className="navbar-start">
           <h1 className="sm:text-lg text-md text-white font-bold uppercase sm:ml-4">
             Colegauno
@@ -45,47 +45,81 @@ const Forum = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <div className="form-control">
-            <input
-              type="text"
-              placeholder="Keresés..."
-              className="input input-bordered w-24 md:w-auto"
-            />
+            <div className="join">
+              <div>
+                <div>
+                  <input
+                    className="input input-bordered join-item"
+                    placeholder="Keresés.."
+                  />
+                </div>
+              </div>
+              <select className="select select-bordered join-item">
+                <option disabled selected>
+                  Szűrők
+                </option>
+                <option>Sci-fi</option>
+                <option>Drama</option>
+                <option>Action</option>
+              </select>
+              <div className="indicator">
+                <button className="btn join-item">Keresés</button>
+              </div>
+            </div>
           </div>
         </div>
         <div className="navbar-end">
-          <a className="btn p-0 sm:p-3 " onClick={toggleExpand}>
+          <a className="btn p-0 sm:p-4 " onClick={toggleExpand}>
             Új poszt
           </a>
         </div>
       </div>
 
       {/*Posts place*/}
-      <div className="bg-white sm:w-3/4 w-5/6 mx-auto pt-7 rounded-xl shadow-lg pb-4">
+      <div className="bg-neutral-content sm:w-3/4 w-1/1 mx-auto pt-7 rounded-xl shadow-lg pb-4">
         {/* hidden searchbar */}
         <div className={styles.hiddenSearch}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Keresés..."
-            className="input input-bordered w-3/4 max-w-96"
-          />
+          <div className="sm:join">
+            <div>
+              <div>
+                <input
+                  className="input input-bordered sm:join-item"
+                  placeholder="Keresés.."
+                />
+              </div>
+            </div>
+            <select className="select select-bordered sm:join-item">
+              <option disabled selected>
+                Szűrők
+              </option>
+              <option>Sci-fi</option>
+              <option>Drama</option>
+              <option>Action</option>
+            </select>
+            <div className="indicator">
+              <button className="btn sm:join-item">Keresés</button>
+            </div>
+          </div>
         </div>
+
         <Poszt
           cim="Poszt címe"
           szoveg="A legjobb poszt a világon, mert itt az ElTerrifico oldalán található.
           Legyél része ennek a szuper, csodálatos szép és mesés oldalnak!"
+          category="Kategória"
         />
         <Poszt
           cim="Poszt címe"
           szoveg="A legjobb poszt a világon, mert itt az ElTerrifico oldalán található.
           Legyél része ennek a szuper, csodálatos szép és mesés oldalnak!"
+          category="Kategória"
         />
       </div>
 
       {/* Toggle card */}
       {isExpanded && (
         <div className="absolute inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-10">
-          <div className="bg-white rounded-lg shadow-md p-4 md:w-1/2 lg:1/3">
+          <div className="bg-neutral-content rounded-lg shadow-md p-4 md:w-1/2 lg:1/3">
             <h3 className="text-lg font-semibold mb-2">Új poszt létrehozása</h3>
             <div className="label">
               <span className="label-text">Cím:</span>
@@ -149,7 +183,10 @@ const Forum = () => {
             </div>
           )}
           {isWarningAlertOpen && (
-            <div role="alert" className="alert alert-error absolute bottom-0 left-0 right-0 text-center mb-4">
+            <div
+              role="alert"
+              className="alert alert-error absolute bottom-0 left-0 right-0 text-center mb-4"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="stroke-current shrink-0 h-6 w-6"
@@ -163,7 +200,9 @@ const Forum = () => {
                   d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              <span>Sikertlene posztolás! Nincsen mindegyik mező kitöltve!</span>
+              <span>
+                Sikertlene posztolás! Nincsen mindegyik mező kitöltve!
+              </span>
             </div>
           )}
         </div>
