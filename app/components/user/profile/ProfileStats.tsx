836ebe355c7b8d847ba79d950eba29bb9435b16d
Fulop-Krisztian <50krissssz22@gmail.com>
2024-02-23 20:17:33 +0100

    Profil oldal statok

---
+++ app/components/user/profile/ProfileStats.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { userData } from '@/app/(user)/profile/ProfilePage'
+
+interface StatProps {
+    userdata: userData | null;
+}
+
+const ProfileStats: React.FC<StatProps> = ({ userdata }) => {
+    if (userdata) {
+        return (
+            <div className="stats grid-flow-row grid-cols-2 sm:stats-horizontal shadow mx-auto overflow-scroll max-h-44">
+
+                <div className="stat">
+                    <div className="stat-title">Szállítási címek</div>
+                    <div className="stat-value">{userdata.ShippingAddress.length}/5</div>
+                    <div className="stat-desc">Maximum 5 fiókonként</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-title">Rendelések</div>
+                    <div className="stat-value">{userdata.Order.length}</div>
+                    <div className="stat-desc">A rendeléseid száma</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-title">Értékelések</div>
+                    <div className="stat-value">{userdata.Review.length}</div>
+                    <div className="stat-desc">Az értékeléseid száma</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-title">Posztok</div>
+                    <div className="stat-value">{userdata.Review.length}</div>
+                    <div className="stat-desc">A posztjaid száma</div>
+                </div>
+
+
+            </div>
+        )
+    }
+
+
+}
+
+export default ProfileStats
