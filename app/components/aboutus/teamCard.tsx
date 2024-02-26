d489949f9b8d9df8e5d71a793aa1949fb51a08f1
TothBarnabas282 <toth.barnabas282@gmail.com>
2024-02-26 21:51:49 +0100

    Rólunk oldal fix

--- app/components/aboutus/teamCard.tsx
+++ app/components/aboutus/teamCard.tsx
@@ -13,16 +13,18 @@ interface TeamMemberCardProps {
 }
 
 const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ name, name2, position, imageUrl, description }) => {
+  const [isHovered, setIsHovered] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
+    setIsHovered(false);
   };
 
   return (
-    <div className="flex-auto f w-full bg-base-100 rounded-lg shadow-md p-4">
-      <Image height={350} width={350} src={imageUrl} alt={name} className="w-24 h-24 rounded-lg mx-auto mb-4" />
-      <h3 className="text-lg font-semibold">{name}</h3>
+    <div className={`card flex-auto w-full bg-base-100 rounded-lg shadow-md p-4 ${isHovered && !isExpanded ? 'hover-effect' : ''}`}  onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <Image height={350} width={350} src={imageUrl} alt={name} className="rounded-lg mx-auto mb-4" />
+      <h3 className="text-lg font-semibold pt-2">{name}</h3>
       <h3 className="text-lg font-semibold mb-2">{name2}</h3>
       <p className="text-sm text-gray-600 mb-2">{position}</p>
       <button onClick={toggleExpand} className="text-green-500 hover:underline mt-2">
@@ -30,8 +32,8 @@ const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ name, name2, position, 
       </button>
       {isExpanded && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-10">
-          <div className="h grid grid-cols-2 m-36 bg-base-100  rounded-lg shadow-md py-7 px-12">
-          <div>
+          <div className="cardExpanded bg-base-100  rounded-lg shadow-md py-7 px-12">
+          <div className="flex items-center">
             <Image src={imageUrl} alt={name} layout="intrinsic" width={400} height={400} className="m-4 rounded-lg img" />
           </div>
             <div className="m-4">
@@ -39,8 +41,8 @@ const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ name, name2, position, 
               <p className="text-sm text-gray-600 mb-2">{position}</p>
               <p className="text-sm">{description}</p>
             </div>
-            <div className="">
-              <button onClick={toggleExpand} className="mt-5 text-green-500 hover:underline">
+            <div className="button">
+              <button onClick={toggleExpand} className="text-green-500 hover:underline">
                 Vissza
               </button>
             </div>
