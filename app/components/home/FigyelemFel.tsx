8e7633606632d30e00567166c8571f1aaf606959
Fulop-Krisztian <50krissssz22@gmail.com>
2024-03-10 18:32:44 +0100

    Pici kódformázás

--- app/components/home/FigyelemFel.tsx
+++ app/components/home/FigyelemFel.tsx
@@ -1,16 +1,18 @@
 function FigyelemFel() {
+
+  const textclass = 'md:text-4xl sm:text-3xl inline-block p-2 text-center text-black font-bold'
   return (
     <div
       className="bg-cover bg-center h-screen flex items-center justify-center"
       style={{ backgroundImage: "url('/HomeKep.jpg')" }}
     >
-      <h1 className="md:text-4xl sm:text-3xl text-black font-bold bg-green-500 inline-block p-2 text-center">
+      <h1 className={`${textclass} bg-green-500`}>
         Éhes vagy?
       </h1>
-      <h1 className="md:text-4xl sm:text-3xl text-black font-bold bg-white inline-block p-2 text-center">
+      <h1 className={`${textclass} bg-white`}>
         Ennél egyet?
       </h1>
-      <h1 className="md:text-4xl sm:text-3xl text-black font-bold bg-red-500 inline-block p-2 text-center">
+      <h1 className={`${textclass} bg-green-500`}>
         Itt a helyed!
       </h1>
     </div>
