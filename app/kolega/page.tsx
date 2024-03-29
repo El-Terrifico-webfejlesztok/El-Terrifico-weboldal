1be5d2f789d21acec4a8e97eaca3922b437c634a
Fulop-Krisztian <50krissssz22@gmail.com>
2024-03-29 16:36:15 +0100

    Kolegák oldal prototípus

---
+++ app/kolega/page.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <div className='grid grid-cols-2 max-w-screen-xl mx-auto'>
+
+
+                <div className="lg:col-span-1 lg:max-h-[calc(50vh-32px)] col-span-2 card bg-base-100 shadow-xl ">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title text-center">New album is released!</h2>
+                        <p>Click the button to listen on Spotiwhy app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="lg:col-span-1 lg:max-h-[calc(50vh-32px)] col-span-2 card bg-base-100 shadow-xl ">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New album is released!</h2>
+                        <p>Click the button to listen on Spotiwhy app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="lg:col-span-1 lg:max-h-[calc(50vh-32px)] col-span-2 card bg-base-100 shadow-xl ">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New album is released!</h2>
+                        <p>Click the button to listen on Spotiwhy app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="lg:col-span-1 lg:max-h-[calc(50vh-32px)] col-span-2 card bg-base-100 shadow-xl ">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">New album is released!</h2>
+                        <p>Click the button to listen on Spotiwhy app.</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Listen</button>
+                        </div>
+                    </div>
+                </div>
+                
+
+
+
+
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default page
